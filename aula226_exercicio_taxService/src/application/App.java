b0409f5cc25d1645d609dca9bff1bcd1b616aca0
Thalles Torres <57546200+ThallesTorres@users.noreply.github.com>
2023-08-21 12:58:47 -0300

    aula226 without interface

---
+++ aula226_exercicio_taxService/src/application/App.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
+public class App {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com os dados do aluguel ");
+        System.out.println("Modelo do carro: ");
+        String carModel = sc.nextLine();
+        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.println("Entre com o preço por hora: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.println("Entre com o preço por dia: ");
+        double pricePerDay = sc.nextDouble();
+        
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("Fatura: ");
+        System.out.println(String.format("Pagamento básico = R$%.2f", cr.invoice.getBasicPayment()));
+        System.out.println(String.format("Imposto = R$%.2f", cr.invoice.getTax()));
+        System.out.println(String.format("Pagamento total = R$%.2f", cr.invoice.getTotalPayment()));
+
+        sc.close();
+    }
+}
